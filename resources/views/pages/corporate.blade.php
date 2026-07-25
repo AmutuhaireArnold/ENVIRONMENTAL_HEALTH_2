121448c63cd789e9410a90ad698f8dbc682c0f2f
AmutuhaireArnold <amutuhairearnold090@gmail.com>
2026-07-25 06:55:45 +0000

    remved linkd

--- resources/views/pages/corporate.blade.php
+++ resources/views/pages/corporate.blade.php
@@ -131,11 +131,12 @@
 <button type="button" class="sister-orgs-nav prev" id="sisterOrgsPrev" aria-label="Scroll Member Associations left">‹</button>
 <div class="sister-orgs-track" id="sisterOrgsTrack">
 
+<a class="sister-org-logo">
 <span class="sister-org-logo-box"><img src="/images/PHS.png" alt="Member Association 1 logo" onerror="this.parentElement.textContent='MA1'"></span>
 <span class="sister-org-name">MMUPHSA</span>
 </a>
 
-<a class="sister-org-logo" href="/associations/sister-org-2">
+<a class="sister-org-logo">
 <span class="sister-org-logo-box"><img src="/images/MUEHSA.png" alt="Member Association 2 logo" onerror="this.parentElement.textContent='MA2'"></span>
 <span class="sister-org-name">MUEHSA</span>
 </a>
